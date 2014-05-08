6a78430e75b717f4fc92c9980e35258a40e01595
Marco La Rosa <marco@larosa.org.au>
2014-05-08 16:18:49 +1000

    Built search from commit 269ffc9 on branch master

--- app/scripts/services/configuration.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('searchApp')
-  .constant('Configuration', {
-      production: 'https://data.esrc.unimelb.edu.au/solr',
-      testing:    'http://data.esrc.info/solr',
-      loglevel:   'DEBUG'
-  });
