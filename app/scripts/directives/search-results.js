f6e3d313a41a3c505f8207b45455f25f3161fd4e
Marco La Rosa <m@lr.id.au>
2014-06-02 19:21:21 +1000

    adjust window height now graph moved

--- app/scripts/directives/search-results.js
+++ app/scripts/directives/search-results.js
@@ -8,7 +8,7 @@ angular.module('searchApp')
       link: function postLink(scope, element, attrs) {
 
           /* Initialise the widget / defaults */
-          scope.height = $window.innerHeight - 250;
+          scope.height = $window.innerHeight - 220;
           scope.showFilters = false;
           scope.site = SolrService.site;
           scope.summaryActive = '';
