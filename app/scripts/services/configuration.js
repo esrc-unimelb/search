7e31c2bf54da7c9fca668f1b6fb52e48838e7b99
Marco La Rosa <m@lr.id.au>
2014-06-12 18:08:23 +1000

    define the allowed route params

--- app/scripts/services/configuration.js
+++ app/scripts/services/configuration.js
@@ -8,7 +8,8 @@
  */
 angular.module('searchApp')
   .constant('Configuration', {
-      production: 'https://data.esrc.unimelb.edu.au/solr',
-      testing:    'https://data.esrc.info/solr',
-      loglevel:   'DEBUG'
+      production:           'https://data.esrc.unimelb.edu.au/solr',
+      testing:              'https://data.esrc.info/solr',
+      loglevel:             'DEBUG',
+      allowedRouteParams:   [ 'q','type', 'function' ]
   });
