db68eaa74336366d36639e4e895799ca2a753dbc
Marco La Rosa <m@lr.id.au>
2014-08-26 10:17:55 +1000

    fix the phrase / keyword search debacle

--- app/scripts/services/solr-service.js
+++ app/scripts/services/solr-service.js
@@ -102,11 +102,10 @@ angular.module('searchApp')
             q = '(name:' + what + '^20 OR altname:' + what + '^10 OR locality:' + what + '^10 OR text:' + what + ')';
         } else {
             if (SolrService.searchType === 'keyword') {
-                //console.log(what);
-                //what = what.replace(' ', ' AND ');
-                q = '(name:' + what + '^100) OR (altname:' + what + '^50) OR (locality:' + what + '^10) OR (text:' + what + ')';
+                what = what.replace(' ', ' AND ');
+                q = 'name:(' + what + ')^100 OR altname:(' + what + ')^50 OR locality:(' + what + ')^10 OR text:(' + what + ')';
             } else {
-                q = '(name:"' + what + '"^100) OR (altname:"' + what + '"^50) OR (locality:"' + what + '"^10) OR (text:"' + what + '")';
+                q = 'name:"' + what + '"^100 OR altname:"' + what + '"^50 OR locality:"' + what + '"^10 OR text:"' + what + '"';
             }
         }
 
