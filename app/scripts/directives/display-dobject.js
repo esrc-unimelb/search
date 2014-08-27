17230d8d82c52df4bd0fa876fbdbb8cf09c9879e
Marco La Rosa <m@lr.id.au>
2014-08-27 19:57:10 +1000

    add large preview to images

--- app/scripts/directives/display-dobject.js
+++ app/scripts/directives/display-dobject.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('searchApp')
-  .directive('displayDobject', function () {
+  .directive('displayDobject', [ '$window', '$document', function ($window, $document) {
     return {
       templateUrl: 'views/display-dobject.html',
       restrict: 'E',
@@ -9,6 +9,35 @@ angular.module('searchApp')
           data: '=ngModel'
       },
       link: function postLink(scope, element, attrs) {
+
+          // bind to the keyup event on the document and ditch the
+          //  preview on escape
+          $document.bind('keyup', function(event) {
+              if (scope.showImage) {
+                  scope.$apply(function() {
+                      if (event.keyCode === 27) {
+                          scope.ditchPreview();
+                      }
+                  });
+              }
+          });
+
+          // ensure none of the large image panels come up
+          scope.showImage = false;
+
+          // figure out some dimensions
+          scope.h = $window.innerHeight;
+          scope.w = $window.innerWidth;
+
+          // user clicked the image - show large version
+          scope.loadImage = function() {
+            scope.showImage = true;
+          }
+
+          // user wants to ditch the large screen
+          scope.ditchPreview = function() {
+              scope.showImage = null;
+          }
       }
     };
-  });
+  }]);
