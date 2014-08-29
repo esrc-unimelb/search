2ce4b130b354b2bf4384e2e581a65f9af4191cd8
Marco La Rosa <m@lr.id.au>
2014-08-29 16:48:21 +1000

    flesh out the image view screen with controls + bugfixes

--- app/scripts/controllers/imageview.js
+++ app/scripts/controllers/imageview.js
@@ -8,11 +8,22 @@ angular.module('searchApp')
         $scope.showImage = null;
 
         $scope.data = ImageService.get($routeParams.imageid);
-        if ($scope.data === undefined) { 
-            $location.url('/'); 
-        }
+        //if ($scope.data === undefined) { 
+        //    $location.url('/'); 
+        //}
         console.log($scope.data);
+        /*
+        $scope.data = {
+            id: 'https://web.esrc.unimelb.edu.au/UMABt/objects/D00000027.htm',
+            name: 'Construction - Glazed atrium roof - Delivery and installation',
+            fullsize: 'https://web.esrc.unimelb.edu.au/UMABt/objects/images/DSC_5177 Construction - Roof folders - Glazed atrium roof - Delivery and installation.JPG',
+            site_name: 'Architecture Building Archive',
+            site_url: 'http://web.esrc.unimelb.edu.au/UMABt'
+        }
+        */
 
+        $scope.height = $window.innerHeight;
+        $scope.width = $window.innerWidth;
         $scope.image_pane_height = $window.innerHeight * 0.9;
         $scope.image_label_height = $window.innerHeight - $scope.image_pane_height;
 
@@ -21,8 +32,13 @@ angular.module('searchApp')
             $scope.$apply(function() {
                 $scope.showLoadingIndicator = null;
                 $scope.showImage = true;
+                $scope.showDetails = true;
             })
         }
         img.src = $scope.data.fullsize;
+
+        $scope.back = function() {
+            $window.history.back();
+        }
 
   }]);
