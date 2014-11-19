b9e4aa3031521f92fc95535d9a1f455010ca4f83
Marco La Rosa <m@lr.id.au>
2014-11-19 13:26:38 +1100

    add option to define how keyword search constructed
    Conflicts:
    	app/scripts/services/configuration.js

--- app/scripts/services/configuration.js
+++ app/scripts/services/configuration.js
@@ -8,12 +8,13 @@
  */
 angular.module('searchApp')
   .constant('Configuration', {
-      production:           'https://solr.esrc.unimelb.edu.au',
-      testing:              'https://data.esrc.info/solr',
-      loglevel:             'DEBUG',
-      deployment:           'production',
-      allowedRouteParams:   [ 'q','type', 'function', 'tag' ],
-      site:                 'UMAB',
-      datasetStart:         '1600-01-01T00:00:00Z',
-      datasetEnd:           '2014-12-31T23:59:59Z'
+      production:               'https://solr.esrc.unimelb.edu.au',
+      testing:                  'https://data.esrc.info/solr',
+      loglevel:                 'DEBUG',
+      deployment:               'testing',
+      allowedRouteParams:       [ 'q','type', 'function', 'level1', 'level2', 'level3' ],
+      site:                     'UMAB',
+      keywordSearchOperator:    'AND',
+      datasetStart:             '2000-01-01T00:00:00Z',
+      datasetEnd:               '2014-12-31T23:59:59Z'
   });
