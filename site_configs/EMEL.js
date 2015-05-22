bdbf0cabb7f0ac6e18c39e91a7cc0dcfa543594d
Marco La Rosa <m@lr.id.au>
2015-05-22 14:45:57 +1000

    Built search from commit 7971610 on branch master

---
+++ site_configs/EMEL.js
@@ -0,0 +1,29 @@
+{
+      "site":                     "EMEL",
+      "deployment":               "production",
+      "debug":                    false,
+      "testing":                  "https://data.esrc.info/solr",
+      "production":               "https://solr.esrc.unimelb.edu.au",
+      "connex":                   "https://connex.esrc.unimelb.edu.au/#/entity",
+      "connexBackend":            "https://cnex.esrc.unimelb.edu.au",
+      "disableConnex":            true,
+      "searchType":               "keyword",
+      "searchTypeKeywordUnion":   "AND",
+      "datasetStart":             "1700-01-01T00:00:00Z",
+      "datasetEnd":               "2015-12-31T23:59:59Z",
+
+      "searchFields": {
+          "name":        { "fieldName": "name",           "displayName": "Entity Name",           "weight": "100" },
+          "altname":     { "fieldName": "altname",        "displayName": "Entity Alternate Name", "weight": "50" },
+          "text":        { "fieldName": "text",           "displayName": "Entity Content",        "weight": "10" },
+          "description": { "fieldName": "description",    "displayName": "Resource Content",      "weight": "1" }
+      },
+
+      "facetWidgets": [
+          { "facetField": "type",       "label": "Entity Type",     "join": "OR" }
+      ],
+
+      "dateFacetWidgets": [
+      ]
+
+}
